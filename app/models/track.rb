e7fca62077776c7a18040b6827507e077a70722e
anup-segu <anup.segu@gmail.com>
2016-03-31 15:30:38 -0700

    Further developed track and album content

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,7 @@
 class Track < ActiveRecord::Base
+  TRACK_TYPES = ['bonus','regular']
   validates :name, :album_id, :ttype, presence: true
-  validates :ttype, inclusion: { in: ['bonus','regular'] }
+  validates :ttype, inclusion: { in: TRACK_TYPES }
 
   belongs_to :album
 end
