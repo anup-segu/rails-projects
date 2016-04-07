f6937a30ab9b238c8133959dce09670a63d02606
anup-segu <anup.segu@gmail.com>
2016-04-06 23:17:26 -0700

    Optimized queries for comments and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,23 @@ class Post < ActiveRecord::Base
 
   has_many :comments
 
+  def all_comments
+    @all_comments ||= self.comments.includes(:author)
+  end
+
+  def order_comments_by_parent_id
+    ordered_comments = Hash.new { |hash,key| hash[key] = [] }
+    all_comments.each do |comment|
+      ordered_comments[comment.parent_comment_id] << comment
+    end
+    ordered_comments
+  end
+
+  def all_comments_by_parent_id
+    @all_comments_by_parent_id ||= order_comments_by_parent_id
+  end
+
+  private
   def has_no_sub?
     self.post_subs.empty?
   end
