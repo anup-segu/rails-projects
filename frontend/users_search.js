740995626b89b873bfa23d1147dceeb7a05a38bf
anup-segu <anup.segu@gmail.com>
2016-04-15 16:30:46 -0700

    add search functionality

--- frontend/users_search.js
+++ frontend/users_search.js
@@ -2,7 +2,7 @@ var UserSearch = function (el) {
   this.$el = $(el);
   this.$input = this.$el.find("input");
   this.$ul = this.$el.find("ul");
-  // this.handleInput.bind(this)
+  // this.handleInput.bind(this)''
   this.$input.on("input", this.handleInput.bind(this));
 };
 
@@ -12,9 +12,8 @@ UserSearch.prototype.handleInput = function() {
     method: "GET",
     url: "/users/search",
     dataType: "json",
-    data: {username: this.$input[0].value},
+    data: {query: this.$input[0].value},
     success: function(resp) {
-      console.log(this.$input[0].value);
       this.renderResults(resp);
     }.bind(this)
   });
@@ -23,9 +22,11 @@ UserSearch.prototype.handleInput = function() {
 UserSearch.prototype.renderResults = function(resp) {
   this.$ul.empty();
 
-  resp.forEach( function() {
-
-  });
+  resp.forEach( function(result) {
+    var $li = $("<li>");
+    $li.text(result.username);
+    this.$ul.append($li);
+  }.bind(this));
 
 };
 
