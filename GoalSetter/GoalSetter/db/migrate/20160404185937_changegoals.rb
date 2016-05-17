51b9e7d5e26319c9dc22db934aafeb38c3155db6
anup-segu <anup.segu@gmail.com>
2016-05-17 12:15:32 -0700

    Clean goal_setter

--- GoalSetter/GoalSetter/db/migrate/20160404185937_changegoals.rb
+++
@@ -1,6 +0,0 @@
-class Changegoals < ActiveRecord::Migration
-  def change
-    add_column :goals, :private, :boolean, default: true
-    add_column :goals, :progress, :string, default: 'Not Started'
-  end
-end
